74ddbe4fe1b2f04ed37dbdfb7df42947d14ca280
SunilCap <152390790+SunilCap@users.noreply.github.com>
2026-09-02 09:56:21 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  * Bump CACHE_VERSION whenever you deploy a new version so returning users get fresh content.
  */
 
-const CACHE_VERSION = 'v1.7.10';
+const CACHE_VERSION = 'v1.8.0';
 const CACHE_NAME    = `blackjack-${CACHE_VERSION}`;
 
 const PRECACHE = [
